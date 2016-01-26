f16f8c88125beccb65cab8d4e774a8ea8398e330
Sudheer Kartan <sk1452@att.com>
2016-01-25 20:35:05 -0600

    Week 3 Assignment from Sudheer Kartan

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates a list from the given matrix to hold the cached value
+makeCacheMatrix <- function(m = matrix()) {
+  mOrg <- NULL
+  mInv <- NULL
+  set <- function(mx) {
+    m <<- mx
+    mOrg <<- NULL
+    mInv <<- NULL
+  }
+  get <- function() m
+  setBoth <- function(Org, Inv) {
+    mOrg <<- Org 
+    mInv <<- Inv
+  }
+  getOrg <- function() mOrg
+  getInv <- function() mInv
+  
+  list(set = set, get = get,
+       setBoth = setBoth,
+       getOrg = getOrg,getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns a matrix that is an inverse of m
+## from the cache, if exists and the cached matrix has not been changed
+## using solve function, if it is not cached
+cacheSolve <- function(m) {
+  mOrg <- m$getOrg()
+  mInv <- m$getInv()
+  mdata <- m$get()
+  if(!is.null(mInv) && identical(mdata, mOrg)) {
+    message("getting cached data")
+    return(mInv)
+  }
+  mInv <- solve(mdata)
+  m$setBoth(mdata, mInv)
+  mInv
 }
